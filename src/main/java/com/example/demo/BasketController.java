463cdb89475fdd874af29eb6a90cc9bdeec6415d
Carnificem <Carnificem1993@gmail.com>
2024-08-12 18:28:33 +0300

    коммит

--- src/main/java/com/example/demo/BasketController.java
+++ src/main/java/com/example/demo/BasketController.java
@@ -3,9 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URL;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/store/order")
@@ -14,15 +12,17 @@ public class BasketController {
     private final BasketService basketService;
 
     @Autowired
-    public BasketController(BasketService basketService){
-        this.basketService=basketService;
+    public BasketController(BasketService basketService) {
+        this.basketService = basketService;
     }
-    @RequestMapping(value = "/add",method = RequestMethod.GET)
-    public void addItems(@RequestParam List<Integer>itemsId){
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public void addItems(@RequestParam List<Integer> itemsId) {
         basketService.addItem(itemsId);
     }
+
     @GetMapping("/get")
-    public List<Integer>getItems(){
+    public List<Integer> getItems() {
         return basketService.getItems();
     }
 }
